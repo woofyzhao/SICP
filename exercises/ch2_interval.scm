259dc625e7e16f17c6a25f5bf130bfc937b55961
woofyzhao(赵子轩) <woofyzhao@tencent.com>
2019-07-04 13:57:57 +0800

    ex

--- exercises/ch2_interval.scm
+++ exercises/ch2_interval.scm
@@ -33,3 +33,19 @@
     (display ",")
     (display (upper-bound x))
     (display ")"))
+
+(define (make-center-width c w)
+    (make-interval (- c w) (+ c w)))
+
+(define (center i)
+    (/ (+ (lower-bound i) (upper-bound i)) 2))
+
+(define (width i)
+    (/ (- (upper-bound i) (lower-bound i)) 2))    
+
+; ex_2.12
+(define (make-center-percent c p)
+    (make-center-width c (* c p)))
+
+(define (percent i)
+    (abs (/ (width i) (center i))))    
