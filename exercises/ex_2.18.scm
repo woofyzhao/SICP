fb4f44ca25a9b682f993688a4a7d2206b7c32b88
woofyzhao(赵子轩) <woofyzhao@tencent.com>
2019-07-08 15:20:42 +0800

    ex

--- exercises/ex_2.18.scm
+++ exercises/ex_2.18.scm
@@ -4,7 +4,7 @@
     (define (iter r next)
         (if (null? next)
             r
-            (cons (cdr next) r)))
+            (iter (cons (car next) r) (cdr next))))
     (iter nil s))
 
 (reverse (list 1 4 9 16 25))
